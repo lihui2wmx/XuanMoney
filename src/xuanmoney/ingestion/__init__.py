be938ae28b751f1db597f6ec7591057a06635008
lihui2wmx <lihui2wmx@163.com>
2026-09-02 11:44:53 +0800

    feat: expose dimensional ingestion API

--- src/xuanmoney/ingestion/__init__.py
+++ src/xuanmoney/ingestion/__init__.py
@@ -1,11 +1,15 @@
 from xuanmoney.ingestion.tabular import (
     TabularIngestionError,
+    dimensional_rows_from_rows,
     income_statements_from_rows,
+    load_dimensional_rows,
     load_income_statements,
 )
 
 __all__ = [
     "TabularIngestionError",
+    "dimensional_rows_from_rows",
     "income_statements_from_rows",
+    "load_dimensional_rows",
     "load_income_statements",
 ]

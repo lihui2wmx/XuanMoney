8f2f773ecb0f216ec52b118cceb884b7faabdcf5
lihui2wmx <lihui2wmx@163.com>
2026-09-02 11:26:36 +0800

    feat: add ingestion package

---
+++ src/xuanmoney/ingestion/__init__.py
@@ -0,0 +1,11 @@
+from xuanmoney.ingestion.tabular import (
+    TabularIngestionError,
+    income_statements_from_rows,
+    load_income_statements,
+)
+
+__all__ = [
+    "TabularIngestionError",
+    "income_statements_from_rows",
+    "load_income_statements",
+]
